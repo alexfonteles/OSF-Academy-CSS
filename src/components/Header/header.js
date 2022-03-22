88d4c89368603db30f6dbd50695adeee4e43a4b0
alexfonteles <allsce@hotmail.com>
2022-03-22 20:47:26 -0300

    exclusão de ob7jetos não utilizados

--- src/components/Header/header.js
+++ src/components/Header/header.js
@@ -1,12 +1,6 @@
 import './header.css'
-import React, {useState} from 'react';
-
-import Dadosdoproduto from '../../mocks/produtos.json';
 
 function Header ({busca, setBusca}) {
-    const [produtos, setprodutos] = useState(Dadosdoproduto);
-   
-    const produtosFiltrados = produtos.filter((produto) => produto.nome.includes(busca))
     
     return <header className='cabecalho'>
     <p>Acordes Shooping</p>
