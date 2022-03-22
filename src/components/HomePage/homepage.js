d4d179e71e6227e90c941d9bef2611f86b08c741
alexfonteles <allsce@hotmail.com>
2022-03-22 20:37:27 -0300

    correcao dos valores nao utilizados

--- src/components/HomePage/homepage.js
+++ src/components/HomePage/homepage.js
@@ -1,22 +1,16 @@
 import './homepage.css';
 import Sidebar from '../Sidebar';
-import React, {useState} from 'react';
 
-import Dadosdoproduto from '../../mocks/produtos.json';
 import CardList from '../CardList/cardlist';
 
 function Homepage() {
-    const [produtos, setprodutos] = useState(Dadosdoproduto);
-   
-    const [busca, setBusca] = useState('');
-    const produtosFiltrados = produtos.filter((produto) => produto.nome.includes(busca))
 
     return (
         <>
     <div className='homepage'>        
         <Sidebar />
         <div className='cards'>
-        <CardList produtos={produtosFiltrados} /> 
+        <CardList /> 
         </div>                     
     </div>
     </>
